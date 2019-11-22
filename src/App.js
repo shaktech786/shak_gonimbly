fe0ea3380e842fb062fdd3bbd7055f021be80bca
Shakeel Bhamani <shakeel.bhamani@gmail.com>
2019-11-22 13:07:44 -0600

    build skeleton for weather app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import 'typeface-roboto';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Paper className="weather">
+          <h1>Is it warm yet?</h1>
+      </Paper>
     </div>
   );
 }
