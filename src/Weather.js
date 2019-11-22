6bb1730339a7916ad55dcac144edf4d83d5c036b
Shakeel Bhamani <shakeel.bhamani@gmail.com>
2019-11-22 16:59:53 -0600

    Adds 5 day weather forecast

--- src/Weather.js
+++ src/Weather.js
@@ -2,17 +2,16 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
-import 'typeface-roboto';
-import './App.css';
 import axios from 'axios';
 import { Card } from '@material-ui/core';
+import "./Weather.css";
 
 class Weather extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             location: "",
-            weather: ""
+            weather: "",
         }
         this.getWeather = this.getWeather.bind(this);
     }
@@ -34,7 +33,7 @@ class Weather extends React.Component {
             .then(response => {
                 axios.get(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${response.data[0].woeid}`)
                     .then(response => {
-                        this.setState({ weather: response.data.consolidated_weather[0] })
+                        this.setState({ weather: response.data })
                     })
                     .catch(error => {
                         console.log(error, "Cannot find weather data for this location")
@@ -46,12 +45,21 @@ class Weather extends React.Component {
     }
 
     render() {
+        const weatherInfo = this.state.weather && this.state.weather.consolidated_weather;
         return (
-            <Paper className="weather">
+            <Paper className="weather-paper">
                 <h1>Is it warm yet?</h1>
                 <Input type="text" onKeyPress={this.onEnter} onChange={this.updateLocation} value={this.state.location} placeholder="City?" />
                 <Button onClick={this.getWeather}>Get Some Weather!</Button>
-                <Card>{this.state.weather.weather_state_name}</Card>
+                {this.state.weather !== "" &&
+                    weatherInfo.map(day => (
+                        <Card className="weather-card">
+                            <h2>{day.applicable_date}</h2>
+                            <img className="weather-icon" alt="Weather Icon" src={`https://www.metaweather.com/static/img/weather/png/${day.weather_state_abbr}.png`} />
+                            <h3>{day.weather_state_name}</h3>
+                        </Card>
+                    ))
+                }
             </Paper>
         );
     }
